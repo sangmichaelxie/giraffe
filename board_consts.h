b732c8861b1dc71f9d9caf7ec884b3b305a78549
Matthew Lai <m@matthewlai.ca>
2014-09-07 09:54:39 +0800

    Added flip array

--- board_consts.h
+++ board_consts.h
@@ -70,6 +70,17 @@ const static uint64_t INVBIT[64] =
 	0xefffffffffffffffULL, 0xdfffffffffffffffULL, 0xbfffffffffffffffULL, 0x7fffffffffffffffULL
 };
 
+const static Square FLIP[64] = {
+	 56,  57,  58,  59,  60,  61,  62,  63,
+	 48,  49,  50,  51,  52,  53,  54,  55,
+	 40,  41,  42,  43,  44,  45,  46,  47,
+	 32,  33,  34,  35,  36,  37,  38,  39,
+	 24,  25,  26,  27,  28,  29,  30,  31,
+	 16,  17,  18,  19,  20,  21,  22,  23,
+	  8,   9,  10,  11,  12,  13,  14,  15,
+	  0,   1,   2,   3,   4,   5,   6,   7
+};
+
 const static uint64_t RANKS[8] = {
 	0x00000000000000ffULL,
 	0x000000000000ff00ULL,
