086145b7dc93614fcb9a6b8514c7b9ef5c855d41
Matthew Lai <m@matthewlai.ca>
2015-03-31 23:41:22 +0100

    Added helper script to push to testing cluster

---
+++ send_to_server.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+	echo "Usage: $0 version_name"
+	exit 1
+fi
+
+VER=$1
+
+TARGET="/vol/bitbucket/ml614/playground/players/$VER"
+
+ssh ic mkdir $TARGET
+
+rsync -av --exclude '*.epd' . ic:$TARGET/
+
+echo $VER > version_tmp.txt
+
+scp -r version_tmp.txt ic:$TARGET/version.txt
+
+rm version_tmp.txt
