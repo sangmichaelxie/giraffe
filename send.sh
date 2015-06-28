1c0a4d12d18402e634d899797d98a96e8daa81ef
Matthew Lai <m@matthewlai.ca>
2015-06-28 13:09:08 +0100

    Added net.dump to ignore

--- send.sh
+++ send.sh
@@ -2,4 +2,4 @@
 
 #rsync -h --progress -z -v -a . batch2:/vol/bitbucket/ml614/tmp/giraffe
 
-rsync -h --progress -z -v -a . hpc:/work/ml614/giraffe
+rsync -h --progress --exclude 'net.dump' -z -v -a . hpc:/work/ml614/giraffe
