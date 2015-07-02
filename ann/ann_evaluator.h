663ffb15f539b43c0b47df36c38c08a84de898cf
Matthew Lai <m@matthewlai.ca>
2015-07-02 02:56:33 +0100

    Fixed a bug in calibrating ANN evaluator

--- ann/ann_evaluator.h
+++ ann/ann_evaluator.h
@@ -76,6 +76,8 @@ public:
 		// (black missing a7 pawn from start position) evaluates to 100
 		Board calibratePosition("rnbqkbnr/1ppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
 
+		m_scaling = 1.0f;
+
 		float v = EvaluateForWhiteImpl(calibratePosition, SCORE_MIN, SCORE_MAX);
 
 		m_scaling = 100.0f / v;
