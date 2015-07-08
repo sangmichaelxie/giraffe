b6ec9b4e63299263b742f5021657c90f9e8d0100
Matthew Lai <m@matthewlai.ca>
2015-07-08 20:26:41 +0100

    Changed send script

--- send.sh
+++ send.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-#rsync -h --progress -z -v -a . batch2:/vol/bitbucket/ml614/tmp/giraffe
+#rsync -h --progress --exclude 'eval.net' --exclude 'trainingResults/*' -z -v -a . batch2:/vol/bitbucket/ml614/tmp/giraffe
 
-#rsync -h --progress --exclude 'net.dump' -z -v -a . hpc:/work/ml614/giraffe
+#rsync -h --progress --exclude 'eval.net' --exclude 'trainingResults/*' -z -v -a . hpc:/work/ml614/giraffe
 
-rsync -h --progress --exclude 'net.dump' -z -v -a . amazon_compute:/data/giraffe
+rsync -h --progress --exclude 'eval.net' --exclude 'trainingResults/*' -z -v -a . amazon_compute:/data/giraffe
